95a4b07afd1174e408f1f7a75936b198a6126f84
volodya iavorskyi <iavorskyi.v.v@gmail.com>
2018-05-11 09:41:28 +0300

    Nope!!! ))))))

--- app/src/androidTest/java/com/mysuperdispatch/test/daxxtestapp/post/list/ListActivityTest.java
+++ app/src/androidTest/java/com/mysuperdispatch/test/daxxtestapp/post/list/ListActivityTest.java
@@ -1,6 +1,5 @@
 package com.mysuperdispatch.test.daxxtestapp.post.list;
 
-
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
